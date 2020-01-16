740a736bf276f5b5062d6f3696d54b480cd60cb7
Paul Kim <pkim1295@gmail.com>
2020-01-16 21:05:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,9 @@ def roll_call_dwarves(array)
   array.each_with_index {|value, index| puts "#{index+1} #{value}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
+  
 end
 
 def long_planeteer_calls# code an argument here
