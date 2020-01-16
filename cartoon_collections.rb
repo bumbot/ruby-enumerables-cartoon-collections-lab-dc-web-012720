ba1339ad3040ef04c165926dfcf2099f092a13ae
Paul Kim <pkim1295@gmail.com>
2020-01-16 21:18:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def long_planeteer_calls(array)
   is_true
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(string_array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
